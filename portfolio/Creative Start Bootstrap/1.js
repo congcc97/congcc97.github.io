980781ddfb567a8a0d926d71c9ffbce75ce3a25d
congcc97 <congcc97@gmail.com>
2018-03-22 16:31:54 +0700

    new commit

---
+++ portfolio/Creative Start Bootstrap/1.js
@@ -0,0 +1,37 @@
+﻿ $(function(){
+    $(window).scroll(function(event) {
+        var heightBody = $('body, html').scrollTop();
+        /*var heightShrink = $('.title-header').offset().top;*/
+        // khong tinh dc ???? bị lỗi chờ sửa?
+
+        if (heightBody >= 170) {
+            $('.menu-top').addClass('shrink');
+        }
+        else{
+            $('.menu-top').removeClass('shrink');
+        }
+
+    });
+
+    // sự kiện click cuộn chuột
+    $('#n1').click(function(event) {
+        $('body, html').animate({scrollTop:$('#about').offset().top}, 800)
+    });
+
+    $('#n2').click(function(event) {
+        $('body, html').animate({scrollTop:$('#services').offset().top}, 800)
+    });
+
+    $('#n3').click(function(event) {
+        $('body, html').animate({scrollTop:$('#portfolio').offset().top}, 800)
+    });
+
+    $('#n4').click(function(event) {
+        $('body, html').animate({scrollTop:$('#contact').offset().top}, 800)
+    });
+
+
+
+
+})
+

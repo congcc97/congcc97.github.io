954c0834c0c10c0db417572a59c2a7a930644b5f
congcc97 <congcc97@gmail.com>
2018-03-24 21:10:05 +0700

    agency bootstrap theme dang hoan thien

---
+++ portfolio/Agency/1.js
@@ -0,0 +1,33 @@
+﻿$(function(){
+
+    $(window).scroll(function(event) {
+        var vitribody = $('body, html').scrollTop();
+
+        if (vitribody >= 300) {
+            $('.menu-top').addClass('shrink');
+        }
+        else {
+            $('.menu-top').removeClass('shrink');
+        }
+
+    });
+
+    // ham croll cho cac nut tren thanh menu
+    $('#n5').click(function(event) {
+        $('body, html').animate({scrollTop:$('#contact').offset().top}, 800);
+    });
+    $('#n4').click(function(event) {
+        $('body, html').animate({scrollTop:$('#team').offset().top}, 800);
+    });
+    $('#n3').click(function(event) {
+        $('body, html').animate({scrollTop:$('#about').offset().top}, 800);
+    });
+    $('#n2').click(function(event) {
+        $('body, html').animate({scrollTop:$('#portfolio').offset().top}, 800);
+    });
+    $('#n1').click(function(event) {
+        $('body, html').animate({scrollTop:$('#services').offset().top}, 800);
+    });
+
+});
+

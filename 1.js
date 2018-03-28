9b7ad72e59c66a880eb0702ef4430e4e2237a3fe
congcc97 <congcc97@gmail.com>
2018-03-28 21:25:43 +0700

    cho xu ly animate CSS va wow.js

--- 1.js
+++ 1.js
@@ -1,4 +1,41 @@
 ﻿$(function(){
 
+    //cuon chuot thay doi menu
+    $(window).scroll(function(event) {
+        var vitriBody = $('body, html').scrollTop();
+        var vitriShrink = $('.heading').offset().top;
+
+        if (vitriBody >= vitriShrink) {
+            $('.menu-top').addClass('shrink');
+        }
+        else {
+            $('.menu-top').removeClass('shrink');
+        }
+    });
+
+    // click cuon chuot tu viet
+
+    $('#n1').click(function(event) {
+        $('body, html').animate({scrollTop:$('#about').offset().top}, 800);
+    });
+
+    $('#n2').click(function(event) {
+        $('body, html').animate({scrollTop:$('#skill').offset().top}, 800);
+    });
+
+    $('#n3').click(function(event) {
+        $('body, html').animate({scrollTop:$('#portfolio').offset().top}, 800);
+    });
+
+    $('#n4').click(function(event) {
+        $('body, html').animate({scrollTop:$('#contact').offset().top}, 800);
+    });
+
+    $('#n5').click(function(event) {
+        $('body, html').animate({scrollTop:$('#contact').offset().top}, 800);
+    });
+
+
+
 })
 
